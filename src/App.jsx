38fbfbc2e0efe63aa38b3c785f49c084d9e07e27
Andres Felipe Campuzano Cervantes <andrescampu1@gmail.com>
2022-03-29 19:30:54 -0500

    Se agrego la funcionalidad de guarda la lista de pasientes en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import {useState} from 'react'
+import {useState,useEffect} from 'react'
 import Formulario from "./components/Formulario"
 import Header from "./components/Header"
 import ListadoPacientes from "./components/ListadoPacientes"
@@ -6,6 +6,18 @@ import ListadoPacientes from "./components/ListadoPacientes"
 function App() {
   const [pacientes,setPacientes] = useState([]);
   const [paciente,setPaciente]=useState({});
+
+  useEffect(() => {
+    const obtenerLs=()=>{
+      const obtenerLs = JSON.parse(localStorage.getItem('pacientes')) ?? [];
+      setPacientes(obtenerLs);
+    }
+    obtenerLs();
+  },[])
+
+  useEffect(() => {
+    localStorage.setItem('pacientes',JSON.stringify( pacientes ));
+  },[pacientes])
 
   const eliminarPaciente=id=>{
     const pacientesActualizado=pacientes.filter(paciente=>paciente.id !== id)
